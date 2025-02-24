0ab8781521ce0771259ac3711566970de6f7bc11
Alexander <avb121@tpu.ru>
2025-02-24 13:04:37 +0700

    Богатырская атмосфера

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,47 @@
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+
+
+from posts.models import Comment, Post, Group, Follow
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        fields = '__all__'
+        model = Post
+        optional_fields = ('group',)
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+        read_only_fields = ('author', 'post')
+        required_fields = ('text',)
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = "__all__"
+        model = Group
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+    following = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = ('user', 'following')
+        model = Follow
+        required_fields = ('following',)
+        optional_fields = ('user',)
